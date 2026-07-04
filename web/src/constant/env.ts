8c7718d2c79c8821aa303fdb8f6d6903739a4e95
csyqlz <csyqlz@users.noreply.github.com>
2026-07-04 21:20:11 +0800

    Release v0.7.2

--- web/src/constant/env.ts
+++ web/src/constant/env.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || "v0.7.1";
+export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || "v0.7.2";
 
 export const DOCS_URL = process.env.NEXT_PUBLIC_DOC_URL || "https://github.com/csyqlz/vozeb/tree/main/docs";
